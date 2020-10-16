9b2c922753ad0bf6ff01935b6845f4ff88600e77
Wilberto Morales <wilbertomorales777@gmail.com>
2020-10-16 11:26:40 -0500

    Remove six import
    Just supporting python3

--- openxmllib/__init__.py
+++ openxmllib/__init__.py
@@ -8,7 +8,7 @@ http://www.ecma-international.org/publications/standards/Ecma-376.htm
 
 import mimetypes
 from io import BytesIO, StringIO
-from six.moves.urllib.request import urlopen
+from urllib.request import urlopen
 
 from . import presentation
 from . import spreadsheet
